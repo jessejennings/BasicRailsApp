e850f4ef742b399502cd71c171e47ed06541c062
jessejennings <jessejenn@gmail.com>
2015-02-19 21:34:56 +0000

    destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,13 +21,26 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)  
     @post.topic = @topic
     authorize @post
-
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
+    end
+  end
+  
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    title = @post.title
+    authorize @post
+    if @post.destroy
+      flash [:notice] = "\"#{title}\" was deleted successfully."
+      redirect_to @topic
+    else 
+      flash[:error] = "There was an error deleting the post."
+      render :show
     end
   end
 
