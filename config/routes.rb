ccd497da3f2334dfc7f14aa430710803d1a19ce6
jessejennings <jessejenn@gmail.com>
2015-02-24 22:38:05 +0000

    Destroy-2nd half

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Bloccit::Application.routes.draw do
 
   devise_for :users
     resources :users, only: [:update]
-    resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments
+      resources :topics do
+        resources :posts, except: [:index] do
+          resources :comments, only: [:create, :destroy]
     end
    end
 
