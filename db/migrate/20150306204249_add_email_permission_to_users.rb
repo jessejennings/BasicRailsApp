47b2f1daa7c2ab61c3416b53b6721604878dbd20
jessejennings <jessejenn@gmail.com>
2015-03-06 23:18:35 +0000

    WIP

--- db/migrate/20150306204249_add_email_permission_to_users.rb
+++ db/migrate/20150306204249_add_email_permission_to_users.rb
@@ -1,5 +1,5 @@
 class AddEmailPermissionToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :email_favorites, :boolean, default: true
+    add_column :users, :email_favorites, :boolean, default: truerails 
   end
 end
