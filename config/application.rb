8aaae5fe19303cbf88c114fd0587b6fbc2762c49
jessejennings <jessejenn@gmail.com>
2015-07-02 21:00:11 -0700

    Change config/enviroments/production.rb
    Change config.assets.compile from false to true

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+#config.assets.intialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -29,6 +30,6 @@ module KnockIt
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    #config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
